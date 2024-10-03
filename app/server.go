2e4cc5b1055169fb56135d85147394b09a1ce021
Cleiton Dantas <cleitondantas@MacBook-Pro-de-Cleiton.local>
2024-10-03 13:42:37 -0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -38,7 +39,21 @@ func main() {
 
 	request := make([]byte, 1024)
 	conn.Read(request)
-	fmt.Println(request)
-	
-	conn.Write([]byte{0,0,0,0,0,0,0,7})
+
+	request_length := int32(binary.BigEndian.Uint32(request[0:4]))
+	request_api_key := int16(binary.BigEndian.Uint16(request[4:6]))
+	request_api_version := int16(binary.BigEndian.Uint16(request[6:8]))
+	correlation_id := int32(binary.BigEndian.Uint32(request[8:12]))
+	client_id := string(request[12:])
+
+	fmt.Printf("request length: %d\n", request_length) //4 bytes
+	fmt.Printf("request_api_key: %d\n", request_api_key) //2 bytes
+	fmt.Printf("request_api_version: %d\n", request_api_version) //2 bytes
+	fmt.Printf("correlation_id: %d\n", correlation_id) //4 bytes
+	fmt.Printf("client_id: %s\n", client_id)
+
+	response := make([]byte, 8)
+	copy(response[4:], request[8:12])
+
+	conn.Write(response)
 }
